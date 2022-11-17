b61c973eeb5214ea381edf24b97368885f0149ee
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2022-11-17 13:10:24 +0330

    migrate from node to deno (#2)

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export { serve } from "https://deno.land/std@0.164.0/http/server.ts";
+export { refresh } from "https://deno.land/x/refresh@1.0.0/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.164.0/fs/expand_glob.ts";
+export { Handlebars } from "https://deno.land/x/handlebars@v0.9.0/mod.ts";
+
+export { transformDirectives } from "npm:@unocss/transformer-directives";
+export { default as MagicString } from "npm:magic-string";
+export * from "npm:unocss";
