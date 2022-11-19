3e82c998bc6f0d4e4c3c5c3ae845e91504c25f19
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2022-11-19 09:42:02 +0330

    feat: replace json with yaml for writing resume (#3)

--- deps.ts
+++ deps.ts
@@ -2,6 +2,7 @@ export { serve } from "https://deno.land/std@0.164.0/http/server.ts";
 export { refresh } from "https://deno.land/x/refresh@1.0.0/mod.ts";
 export { expandGlob } from "https://deno.land/std@0.164.0/fs/expand_glob.ts";
 export { Handlebars } from "https://deno.land/x/handlebars@v0.9.0/mod.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.165.0/encoding/yaml.ts";
 
 export { transformDirectives } from "npm:@unocss/transformer-directives";
 export { default as MagicString } from "npm:magic-string";
