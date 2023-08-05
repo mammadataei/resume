f16e28c34e51391a412a869b503e0a0ff71869e9
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2023-08-05 22:35:47 +0330

    chore: update resume template (#11)

--- template/components/contact.tsx
+++ template/components/contact.tsx
@@ -6,7 +6,7 @@ export function Contact({ contact }: { contact: Contact }) {
 
   return (
     <div class="">
-      <Header title="Contact Info" />
+      <Header title="Contact" />
       <p>{email}</p>
       <p>
         {address} {phone}
