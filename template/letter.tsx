208b4b643c3c7c59f4afff628c224101131ed563
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2024-01-11 21:48:47 +0330

    feat: update letter template (#14)

--- template/letter.tsx
+++ template/letter.tsx
@@ -1,5 +1,3 @@
-import { Contact } from "./components/contact.tsx";
-import { Header } from "./components/header.tsx";
 import type { Letter } from "../schema/types.ts";
 import { loadCoverLetter } from "../utils/loadResume.ts";
 
@@ -9,42 +7,47 @@ export async function Letter(): Promise<JSX.Element> {
   const { title, name, contact, company, recipient, body } = letter;
 
   return (
-    <div class="font-sans font-normal text-xs text-slate-800 leading-relaxed tracking-wide">
-      <div class="flex pt-6">
-        <div class="w-1/3 space-y-8 pr-6">
-          <div class="space-y-1">
-            <Header title={title} />
-            <h1 class="text-4xl font-bold text-stone-700">{name}</h1>
-          </div>
+    <div class="space-y-8 font-sans font-normal text-xs leading-relaxed tracking-wide">
+      <header class="space-y-0.5 text-slate-700">
+        <h1 class="text-2xl font-bold text-sky-600">{name}</h1>
+        <h2 class="text-sm font-medium">{title}</h2>
 
-          <Contact contact={contact} />
+        <div class="pt-1">
+          <p>Phone: {contact.phone} </p>
+          <p>
+            Email:{" "}
+            <a class="underline" href={`mailto:${contact.email}`}>
+              {contact.email}
+            </a>
+          </p>
+        </div>
+      </header>
+
+      <main class="w-3/4 space-y-8 text-sm leading-relaxed tracking-wide text-slate-800">
+        <p>{new Date().toDateString()}</p>
+
+        <div>
+          <p>
+            {recipient.firstName} {recipient.lastName}
+          </p>
+          <p>{recipient.title}</p>
+          <p>{company}</p>
         </div>
 
-        <div class="w-2/3 pl-6 space-y-8 text-sm leading-relaxed tracking-wide text-slate-700">
-          <p>{new Date().toDateString()}</p>
-
-          <div class="">
-            <p>
-              {recipient.firstName} {recipient.lastName}
-            </p>
-            <p>{recipient.title}</p>
-            <p>{company}</p>
-          </div>
-
+        <div class="space-y-2">
+          <p>Dear {recipient.firstName},</p>
           <div class="space-y-2">
-            <p>Dear {recipient.firstName},</p>
-            <div class="space-y-2">
-              {body.map((paragraph) => <p>{paragraph}</p>)}
-            </div>
-          </div>
-
-          <div>
-            <p>Regards,</p>
-            <p>{name}</p>
-            <p>{title}</p>
+            {body.map((paragraph) => (
+              <p>{paragraph}</p>
+            ))}
           </div>
         </div>
-      </div>
+
+        <div>
+          <p>Regards,</p>
+          <p>{name}</p>
+        </div>
+      </main>
     </div>
   );
 }
